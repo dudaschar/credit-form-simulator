71b8f15f66366d0d9aae164d117aa62f8858dea2
Eduarda Scharnhorst <eduardascharnhorst@gmail.com>
2018-08-04 14:23:45 -0300

    SCSS installed

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => (
+  <div className="Navbar">
+    navbar
+  </div>
+)
+
+export default Navbar
---
+++ src/pages/signup/Signup.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from '../../components/navbar/Navbar'
+
+const Signup = () => (
+  <div className="signup">
+    <Navbar />
+  </div>
+)
+
+export default Signup
