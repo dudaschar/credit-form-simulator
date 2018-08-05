7971f541cf3f1a3a5a25d027c4abee35b5dbd117
Eduarda Scharnhorst <eduardascharnhorst@gmail.com>
2018-08-04 21:05:33 -0300

    Product component with props

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,29 +1,38 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import './product.css'
 
-const Product = () => (
+const Product = ({name, cpf, value, deadline, goal}) => (
   <div className="product">
     <div className="product__customer">
       <h4 className="product__title">Me chamo:</h4>
-      <p className="product__response">Paul Irish</p>
+      <p className="product__response">{name}</p>
       <p className="product__s-number">
-        <span className="product__title">CPF: </span>762.888.176-92
+        <span className="product__title">CPF: </span>{cpf}
       </p>
     </div>
     <div className="product__price">
       <h4 className="product__title">Preciso de:</h4>
-      <p className="product__response">R$2.000</p>
+      <p className="product__response">R${value}</p>
     </div>
     <div className="product__payment">
       <h4 className="product__title">Quero Pagar em</h4>
-      <p className="product__response">12 vezes</p>
+      <p className="product__response">{deadline} vezes</p>
     </div>
     <div className="product__goal">
       <h4 className="product__title">Para:</h4>
-      <p className="product__response">Comprar uma bike</p>
+      <p className="product__response">{goal}</p>
     </div>
    
   </div>
 )
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  cpf: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  deadline: PropTypes.string.isRequired,
+  goal: PropTypes.string.isRequired,
+}
 
 export default Product
--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -5,7 +5,13 @@ import Product from '../../components/product/Product'
 const Signup = () => (
   <div className="signup">
     <Navbar />
-    <Product />
+    <Product
+      name="Paul Irish"
+      cpf="762.888.176-92"
+      value="2.000"
+      deadline="12"
+      goal="Comprar uma bike"
+    />
   </div>
 )
 
