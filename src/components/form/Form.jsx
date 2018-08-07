889010dde3f9765c7c8215f4d790540f9883be8c
Eduarda Scharnhorst <eduardascharnhorst@gmail.com>
2018-08-06 22:14:07 -0300

    Dropdown with options

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Fetch } from 'react-data-fetching'
+import MaskedInput from 'react-text-mask'
 import "./form.css"
 import jsonData from "./form-content"
 
@@ -10,36 +11,35 @@ class Form extends React.Component {
       rgNumber: '',
       rgDate: '',
       rgDep: 'select',
-      department: [],
       gender: '',
+      numberValid: false,
+      dateValid: false,
+      DepValid: false,
+      genderValid: false,
     }
     this.handleChange = this.handleChange.bind(this)
+    this.fetchData = this.fetchData.bind(this)
   }
-
-  // renderOptions = ({ data }) => {
-  //   let options = []
-  //   for (let i = 0; i < data.length; i++) {
-  //     options.push(
-  //       <option value={data[i].value}>
-  //         {data[i].label}
-  //       </option>
-  //     )
-  //   }
-  //   return options
-  // }
 
   handleChange(event) {
     this.setState({ [event.target.name]: event.target.value })
-    console.log([event.target.name], event.target.value)
   }
 
-  // componentDidMount() {
-  //   let content
-  //   fetch('./form-content.json')
-  //   .then( response => {
-  //     return response.json()
-  //   }).then( console.log())
-  // }
+  fetchData() {
+    let data
+    let options = []
+
+    fetch({jsonData})
+    .then(data = jsonData)
+  
+    for (let i = 0; i < data.orgao_emissor.length; i++) {
+      options.push(
+        <option key={i} value={data.orgao_emissor[i].value}>{data.orgao_emissor[i].label}</option>
+      ) 
+    }
+    return options
+  }
+  
 
   render() {
     return (
@@ -58,35 +58,31 @@ class Form extends React.Component {
           </div>
           <div className="form__rg--input">
             <label htmlFor="d-rg">Data de emissão</label>
-            <input
+            <MaskedInput
+              mask={[/\d/, /\d/, '/', /\d/, /\d/, '/', /\d/, /\d/, /\d/, /\d/]}
               value={this.state.rgDate}
+              guide={false}
+              d="date-rg"
               onChange={this.handleChange}
-              id="date-rg"
-              type="text"
               name="rgDate"
+              type="text"
             />
             <span>-</span>
           </div>
           <div className="form__rg--input">
-              <label htmlFor="e-rg">Órgão expedidor</label>
-              <select
-                id="dep-rg"
-                name="rgDep"
-              >
-                <option value="select">
-                  Selecione
-                </option>
-                {/* <Fetch
-                  url="./form-content"
-                  render={this.renderOptions}
-                >
-                </Fetch> */}
-              </select>
+            <label htmlFor="e-rg">Órgão expedidor</label>
+            <select
+              id="dep-rg"
+              name="rgDep"
+              onChange={this.handleChange}
+            >
+              {this.fetchData()}
+            </select>
            
           </div>
         </div>
         <div className="form__gender">
-          <p>Gênero</p>
+          <p className="form__gender--title">Gênero</p>
           <label htmlFor="g-man">Masculino</label>
           <input
             checked={this.state.gender === "Masculino"}
